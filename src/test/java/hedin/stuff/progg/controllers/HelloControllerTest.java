01c6ca6b760d8b14fa59154fb07ee4dffb3edfbc
hodden <hedin@kth.se>
2015-04-03 22:25:00 +0200

    Added TestCase for HelloController
    Added first testcase for HelloController

---
+++ src/test/java/hedin/stuff/progg/controllers/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package hedin.stuff.progg.controllers;
+
+import java.util.HashMap;
+
+import junit.framework.TestCase;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class HelloControllerTest extends TestCase {
+
+	private final Gson gson;
+	
+	public HelloControllerTest(){
+		super();
+		gson = (new GsonBuilder()).setPrettyPrinting().create();
+	}
+	
+	public void testWorld(){
+		HashMap<String,String> map = new HashMap<String,String>();
+		map.put("Hello","World");
+		map.put("Presented by", "Patrik & THE king of Sweden Niclas");
+		String expectedResult = gson.toJson(map);
+		HelloController controller = new HelloController();
+		String s = controller.world();
+		 
+		assertEquals(expectedResult, s);
+	}
+}
