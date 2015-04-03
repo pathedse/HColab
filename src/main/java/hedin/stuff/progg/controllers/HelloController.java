e3473bcf8eed581214e466e7f9f45dc0e6143df9
pathedse <pathed.se@gmail.com>
2015-04-03 21:12:15 +0200

    Initial Commit
    Project setup

---
+++ src/main/java/hedin/stuff/progg/controllers/HelloController.java
@@ -0,0 +1,34 @@
+package hedin.stuff.progg.controllers;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@Controller()
+@RequestMapping("/hello")
+public class HelloController {
+	
+	private final Gson gson;
+	
+	public HelloController(){
+		gson = (new GsonBuilder()).setPrettyPrinting().create();
+	}
+	
+	
+	@RequestMapping(value="world", method=RequestMethod.GET)
+	@ResponseBody
+	public String world(){
+		HashMap<String,String> map = new HashMap<String,String>();
+		map.put("Hello","World");
+		map.put("Presented by", "Patrik");
+		
+		return gson.toJson(map);
+	}
+
+}
